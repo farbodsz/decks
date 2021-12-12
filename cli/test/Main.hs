06d15c489cd81f0893268a2310dc73c7ffddb4b7
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2021-12-12 13:09:47 +0000

    test: limit allowed chars in identifier characters

--- cli/test/Main.hs
+++ cli/test/Main.hs
@@ -15,5 +15,12 @@ main = hspec $ do
     describe "pIdentifier" $ do
         it "can parse simple identifiers" $ do
             parse pIdentifier "" "fooBar" `shouldParse` Identifier "fooBar"
+        it "can contain dashes and underscores" $ do
+            parse pIdentifier "" "foo_bar-Foo"
+                `shouldParse` Identifier "foo_bar-Foo"
+        it "should not contain braces" $ do
+            parse pIdentifier "" `shouldFailOn` "fo{o}Bar"
+        it "should not start with a number" $ do
+            parse pIdentifier "" `shouldFailOn` "4ooBar"
 
 --------------------------------------------------------------------------------
