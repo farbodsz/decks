23bde83b03fd6b67942cfa55b3ac95a1f232099e
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2021-12-15 17:28:16 +0000

    feat(codegen): support let bindings

--- cli/src/Decks/CodeGen.hs
+++ cli/src/Decks/CodeGen.hs
@@ -8,10 +8,13 @@ import           Decks.Error
 import           Decks.Grammar
 import           Decks.Logging
 
+import           Control.Applicative            ( (<|>) )
 import           Control.Monad.Trans.State
 
-import           Data.Functor
+import           Data.Functor                   ( (<&>) )
 import qualified Data.HashMap.Lazy             as M
+import           Data.List                      ( union )
+import           Data.Maybe
 import           Data.Text                      ( Text )
 import qualified Data.Text                     as T
 import qualified Data.Text.IO                  as TIO
@@ -24,28 +27,69 @@ import qualified Data.Text.IO                  as TIO
 type Html = Text
 type HtmlResult = Either CodeGenError Html
 
-type DefinitionMap = M.HashMap Identifier ContentTemplate
--- type VariableMap = M.HashMap Identifier DecksElement -- TODO:
+-- | Represents a content template with some default HTML attributes, used for
+-- let bindings, awaiting conversion to a HTML result.
+--
+-- Let bindings essentially bind an identifier to an existing definition,
+-- possibly with some attributes set. For example, in the following Decks
+-- program, example refers to a type of text with certain attributes and
+-- content:
+-- @
+-- !def text = { <p $attrs$>$content$</p> }
+-- !let example = text [.my-class] { Hello World! }
+-- example
+-- example [.overridden-style] { Overridden content }
+-- @
+--
+-- The initial attributes cannot be stored as HTML, its final stage, since they
+-- can still be overridden (L4 of example), hence @DecksAttr@ from the grammar
+-- is used.
+--
+data PendingContentTemplate = PendingContentTemplate
+    { pctTemplate :: ContentTemplate
+    -- ^ The template text with template strings like @$content$@..
+    , pctAttrs    :: [DecksAttr]
+    -- ^ Attributes waiting to be applied to the template.
+    , pctContent  :: Maybe Content
+    -- ^ Content waiting to be applied to the template.
+    }
+
+updatePct
+    :: PendingContentTemplate     -- ^ Initial 'PendingContentTemplate'.
+    -> [DecksAttr]                -- ^ Attributes overriding the initial ones.
+    -> Maybe Content              -- ^ Content overriding the initial one.
+    -> PendingContentTemplate     -- ^ Resulting 'PendingContentTemplate'.
+updatePct PendingContentTemplate {..} as mc =
+    PendingContentTemplate pctTemplate (as `union` pctAttrs) (mc <|> pctContent)
+
+type VariableMap = M.HashMap Identifier PendingContentTemplate
 
 data DecksState = DecksState
-    { stDefinitions :: DefinitionMap
-    -- , stVariables :: VariableMap
+    { stDefinitions :: VariableMap
+    , stLetBindings :: VariableMap
     }
 
 initDecksState :: DecksState
-initDecksState = DecksState M.empty
+initDecksState = DecksState M.empty M.empty
 
 insertDef :: Identifier -> ContentTemplate -> DecksState -> DecksState
-insertDef i ct (DecksState defs) = DecksState $ M.insert i ct defs
+insertDef i ct (DecksState defs lets) = DecksState (M.insert i pct defs) lets
+    where pct = PendingContentTemplate ct [] Nothing
+
+insertLet :: Identifier -> PendingContentTemplate -> DecksState -> DecksState
+insertLet i t (DecksState defs lets) = DecksState defs (M.insert i t lets)
 
 -- | Returns True if the identifier has already been defined in a prior program
 -- statement.
 alreadyDefined :: Identifier -> DecksState -> Bool
-alreadyDefined i (DecksState defs) = M.member i defs
+alreadyDefined i (DecksState defs lets) = M.member i defs || M.member i lets
 
 -- | Retrieves an identifier, from some type of prior statement.
-getIdentifier :: Identifier -> DecksState -> Maybe ContentTemplate
-getIdentifier i (DecksState defs) = M.lookup i defs
+getIdentifier :: Identifier -> DecksState -> Maybe PendingContentTemplate
+getIdentifier i (DecksState defs lets) = mapMaybeFirst (M.lookup i) srcs
+  where
+    mapMaybeFirst f = listToMaybe . mapMaybe f
+    srcs = [defs, lets]
 
 
 --------------------------------------------------------------------------------
@@ -72,33 +116,38 @@ genProgram (DecksProgram stmts) = mapM genStmt stmts <&> combineResults
 
 -- | Generates HTML output from a Decks statement.
 genStmt :: DecksStmt -> State DecksState HtmlResult
-genStmt (DecksDrawStmt el ) = genElement el
-genStmt (DecksDefStmt i ct) = whenIdentValid i (insertDef i ct)
-genStmt _                   = pure . Left . InternalError $ "Not implemented"
+genStmt (DecksDrawStmt el) = genElement el
+genStmt (DecksDefStmt i ct) = whenIdentUniq i (modify $ insertDef i ct)
+genStmt (DecksLetStmt i DecksElement {..}) = whenIdentUniq i $ do
+    m_pct <- gets $ getIdentifier elIdent
+    case m_pct of
+        Nothing  -> pure $ Left $ UndefinedIdentifier elIdent
+        Just pct -> withState
+            (insertLet i (updatePct pct elAttrs elContent))
+            (pure $ Right (mempty :: Text))
 
--- | 'whenIdentifierValid' @identifier modifyFunction@ applies the function to
+-- | 'whenIdentUniq' @identifier modifyFunction@ applies the function to
 -- the state if the identifier does not already exist, otherwise returns an
 -- error.
-whenIdentValid
-    :: Identifier -> (DecksState -> DecksState) -> State DecksState HtmlResult
-whenIdentValid ident f = get >>= \st -> if alreadyDefined ident st
+whenIdentUniq
+    :: Identifier -> State DecksState a -> State DecksState HtmlResult
+whenIdentUniq ident f = get >>= \st -> if alreadyDefined ident st
     then pure $ Left $ MultipleDefinitions ident
-    else modify f >> pure (Right mempty)
+    else f >> pure (Right mempty)
 
 genElement :: DecksElement -> State DecksState HtmlResult
-genElement (DecksElement i as mc) = gets (getIdentifier i) <&> \case
-    Nothing -> Left $ UndefinedIdentifier i
-    Just ct -> fillContentTemplate as mc ct
+genElement DecksElement {..} = gets (getIdentifier elIdent) <&> \case
+    Nothing  -> Left $ UndefinedIdentifier elIdent
+    Just pct -> fillContentTemplate $ updatePct pct elAttrs elContent
 
 -- | Returns the HTML representing a filled-in content template.
-fillContentTemplate
-    :: [DecksAttr] -> Maybe Content -> ContentTemplate -> HtmlResult
-fillContentTemplate as mc (ContentTemplate ct) = do
+fillContentTemplate :: PendingContentTemplate -> HtmlResult
+fillContentTemplate (PendingContentTemplate (ContentTemplate ct) as mc) = do
     attrs   <- fillCtAttrs as
     content <- maybe (Left EmptyContent) (Right . unContent) mc
     pure . T.replace "$attrs$" attrs . T.replace "$content$" content $ ct
 
-data AttrHtmlResults = AttrHtmlResults
+data HtmlAttributes = HtmlAttributes
     { attrIdent   :: HtmlResult
     , attrClasses :: HtmlResult
     , attrStyles  :: HtmlResult
@@ -109,13 +158,13 @@ data AttrHtmlResults = AttrHtmlResults
 fillCtAttrs :: [DecksAttr] -> HtmlResult
 fillCtAttrs = attrsToHtml . processAttributes
   where
-    attrsToHtml :: AttrHtmlResults -> HtmlResult
-    attrsToHtml AttrHtmlResults {..} =
+    attrsToHtml :: HtmlAttributes -> HtmlResult
+    attrsToHtml HtmlAttributes {..} =
         fmap (T.strip . T.unwords)
             . sequenceA
             $ [attrIdent, attrClasses, attrStyles]
 
-    processAttributes :: [DecksAttr] -> AttrHtmlResults
+    processAttributes :: [DecksAttr] -> HtmlAttributes
     processAttributes as =
         let isId (CssId _) = True
             isId _         = False
@@ -124,10 +173,10 @@ fillCtAttrs = attrsToHtml . processAttributes
             isStyle (CssProp _ _) = True
             isStyle _             = False
             process converter predicate = converter . filter predicate $ as
-        in  AttrHtmlResults { attrIdent   = process idsToHtml isId
-                            , attrClasses = process classesToHtml isClass
-                            , attrStyles  = process stylesToHtml isStyle
-                            }
+        in  HtmlAttributes { attrIdent   = process idsToHtml isId
+                           , attrClasses = process classesToHtml isClass
+                           , attrStyles  = process stylesToHtml isStyle
+                           }
 
     idsToHtml :: [DecksAttr] -> HtmlResult
     idsToHtml []        = Right ""
