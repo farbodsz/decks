af6437f4733e62135feb5fd70d8093033be06e38
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2021-12-17 16:28:04 +0000

    chore(web): add initial web editor skeleton

---
+++ web/README.md
@@ -0,0 +1,3 @@
+# Web editor (WYSIWYG)
+
+This directory contains the source code for the WYSIWYG editor of Decks.
---
+++ web/src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <p>Decks Editor</p>;
+}
+
+export default App;
