3b30565a508e771d3ab5bb35fa013c2fbec8687d
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2021-12-17 19:05:20 +0000

    feat(cli): add default options for input/output paths

--- cli/src/Decks/Commands.hs
+++ cli/src/Decks/Commands.hs
@@ -21,14 +21,19 @@ parseCmd = execParser $ info (pOpts <**> helper) (fullDesc <> header "Decks")
 pOpts :: Parser Opts
 pOpts =
     Opts
-        <$> argument
-                str
-                (metavar "PATH" <> showDefault <> value "index.html" <> help
-                    "Directory path containing a Decks file to parse."
+        <$> strOption
+                (  long "input"
+                <> metavar "INPUT_DIR"
+                <> showDefault
+                <> value defaultInputDir
+                <> help "Directory path containing a Decks file to parse"
                 )
         <*> strOption
-                (long "out" <> metavar "OUTPUT" <> help
-                    "Path for the output HTML file"
+                (  long "output"
+                <> metavar "OUTPUT_FILE"
+                <> showDefault
+                <> value defaultOutputFile
+                <> help "Destination of the output HTML file"
                 )
         <*> (not <$> switch
                 (long "no-watch" <> short 'n' <> help
@@ -39,5 +44,13 @@ pOpts =
                 (long "verbose" <> short 'v' <> help
                     "Verbose mode prints the generated AST and HTML"
                 )
+
+--------------------------------------------------------------------------------
+
+defaultInputDir :: FilePath
+defaultInputDir = "."
+
+defaultOutputFile :: FilePath
+defaultOutputFile = "index.html"
 
 --------------------------------------------------------------------------------
