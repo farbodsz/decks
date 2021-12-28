0088625ee0bb575d13ffe61946ae5c3700e4e05d
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2021-12-28 19:10:47 +0000

    fix(web): add load button to fetch from backend

---
+++ web/run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+npx http-server -c-1 -o --cors --port 3000
--- web/src/index.ts
+++ web/src/index.ts
@@ -1,10 +1,48 @@
-Reveal.initialize({});
-console.log("Reveal initialized");
+const BACKEND_URL = "http://localhost:8081/decks";
 
 /**
  * Editing mode
  */
 let editorEditable = false;
+
+function revealInit() {
+  Reveal.initialize({
+    // Disables the default reveal.js slide layout (scaling and centering)
+    // so that you can use custom CSS layout
+    // NOTE: Typings not up-to-date so some fields flagged as error.
+    // See https://github.com/DefinitelyTyped/DefinitelyTyped/pull/57863
+    // @ts-ignore
+    disableLayout: true,
+  });
+  console.log("Reveal initialized");
+}
+
+function revealRefresh(currSlide: { h: number; v: number }) {
+  Reveal.sync();
+  Reveal.slide(currSlide.h, currSlide.v);
+}
+
+/**
+ * Replaces the HTML presentation with the one loaded from the backend.
+ */
+function editorLoadContent() {
+  console.log("Loading content");
+  const currSlide = Reveal.getIndices();
+
+  fetch(BACKEND_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      const contentContainer = document.getElementById("editor-content");
+      if (!contentContainer) {
+        console.error("No content container found");
+        return;
+      }
+
+      contentContainer.innerHTML = data;
+      revealRefresh(currSlide);
+    })
+    .catch((error) => console.error("Error " + error.message));
+}
 
 /**
  * Toggles the editing mode (edit/preview) and updates UI elements reflecting
@@ -31,6 +69,8 @@ function editorToggleEdit() {
  * Outputs the presentation HTML
  */
 function editorSave() {
-  const content = document.querySelector(".reveal .slides")?.innerHTML;
+  const content = document.getElementById("editor-content")?.innerHTML;
   console.log(content);
 }
+
+revealInit();
